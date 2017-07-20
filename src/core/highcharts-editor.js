360037f9efa1a7233fc57273a98b2be2b4b22d92
cvasseng <cvasseng@gmail.com>
2017-07-20 12:56:31 +0200

    Linting

--- src/core/highcharts-editor.js
+++ src/core/highcharts-editor.js
@@ -26,7 +26,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 //////////////////////////////////////////////////////////////////////////////
 
-/** The main highcharts editor namespace 
+/** The main highcharts editor namespace
  * @ignore
  */
 var highed = {
@@ -56,7 +56,7 @@ var highed = {
         document.body.removeChild(l);
     },
 
-    /** Clear an object 
+    /** Clear an object
       * Deletes all the object attributes.
       * Useful when needing to clear an object without invalidating references to it
       * @namespace highed
@@ -68,7 +68,7 @@ var highed = {
         });
     },
 
-    /** Preform an AJAX request. Same syntax as jQuery. 
+    /** Preform an AJAX request. Same syntax as jQuery.
      *  @namespace highed
      *  @param p {object} - options
      *    > url {string} - the URL to call
@@ -78,8 +78,8 @@ var highed = {
      *    > error {function} - function to call on request fail
      *    > data {object} - the payload
      *    > autoFire {boolean} - whether or not to fire the request right away
-     * 
-     *   @emits Error {string} - when there's an error 
+     *
+     *   @emits Error {string} - when there's an error
      *   @emits OK {string} - when the request succeeded
      *   @returns {object} - interface to the request
      */
@@ -116,12 +116,12 @@ var highed = {
         r.onreadystatechange = function () {
             events.emit('ReadyStateChange', r.readyState, r.status);
 
-            if (r.readyState === 4 && r.status === 200) {         
-              if (props.dataType === 'json') {        
+            if (r.readyState === 4 && r.status === 200) {
+              if (props.dataType === 'json') {
                 try {
                   var json = JSON.parse(r.responseText);
                   if (highed.isFn(props.success)) {
-                    props.success(json);        
+                    props.success(json);
                   }
                   events.emit('OK', json);
                 } catch(e) {
@@ -129,13 +129,13 @@ var highed = {
                     props.error(e.toString(), r.responseText);
                   }
                   events.emit('Error', e.toString(), r.status);
-                }      
+                }
               } else {
                 if (highed.isFn(props.success)) {
                   props.success(r.responseText);
-                }        
+                }
                 events.emit('OK', r.responseText);
-              }         
+              }
             } else if (r.readyState === 4) {
               events.emit('Error', r.status, r.statusText);
               if (highed.isFn(props.error)) {
@@ -146,14 +146,14 @@ var highed = {
 
         function fire() {
             try {
-              r.send(JSON.stringify(props.data));            
+              r.send(JSON.stringify(props.data));
             } catch (e) {
-              r.send(props.data || true);      
-            }    
+              r.send(props.data || true);
+            }
         }
 
         if (props.autoFire) {
-            fire();    
+            fire();
         }
 
         return {
@@ -163,19 +163,19 @@ var highed = {
         };
     },
 
-    /** Generate a uuid 
+    /** Generate a uuid
      *  Borrowed from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
      *  @namespace highed
      *  @returns {string} - a UUID string
      */
     uuid: function () {
         var d = new Date().getTime(), uuid;
-        
-        if (window.performance && typeof window.performance.now === "function") {
+
+        if (window.performance && typeof window.performance.now === 'function') {
             d += window.performance.now(); //use high-precision timer if available
         }
-        
-        uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+
+        uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = (d + Math.random() * 16) % 16 | 0;
             d = Math.floor(d / 16);
             return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
@@ -232,10 +232,10 @@ var highed = {
                 {
 
                 } else {
-                    s += ' ';                    
+                    s += ' ';
                 }
             }
-            s += str[i];            
+            s += str[i];
         }
 
         return s[0].toUpperCase() + s.substr(1);
@@ -365,25 +365,25 @@ var highed = {
 
         path = path.replace(/\-\-/g, '.').replace(/\-/g, '.').split('.');
 
-        path.forEach(function(p, i) {
-            if (i === path.length - 1) {  
-                current[p] = value;                                     
+        path.forEach(function (p, i) {
+            if (i === path.length - 1) {
+                current[p] = value;
             } else {
                 if (typeof current[p] === 'undefined') {
                     current = current[p] = {};
                 } else {
-                    current = current[p]; 
+                    current = current[p];
 
                     if (highed.isArr(current)) {
                         if (index > current.length - 1) {
                             for (var j = current.length; j <= index; j++ ) {
                                 current.push({});
                             }
-                        } 
-                        if (index >= 0) {
-                            current = current[index];                            
                         }
-                    }                      
+                        if (index >= 0) {
+                            current = current[index];
+                        }
+                    }
                 }
             }
         });
@@ -412,20 +412,20 @@ var highed = {
 
         path = path.replace(/\-\-/g, '.').replace(/\-/g, '.').split('.');
 
-        path.forEach(function(p, i) {
-            if (i === path.length - 1) {  
+        path.forEach(function (p, i) {
+            if (i === path.length - 1) {
                 if (typeof current !== 'undefined') {
-                    result = current[p];                                     
-                } 
+                    result = current[p];
+                }
             } else {
                 if (typeof current[p] === 'undefined') {
                     current = current[p] = {};
                 } else {
-                    current = current[p];         
+                    current = current[p];
 
                     if (highed.isArr(current) && index >= 0 && index < current.length) {
                         current = current[index];
-                    }               
+                    }
                 }
             }
         });
@@ -465,7 +465,7 @@ var highed = {
                    a[bk].push(highed.merge(highed.isArr(i) ? [] : {}, i));
                  }
                 });
-            } else if (b[bk].tagName && b[bk].appendChild && b[bk].removeChild && b[bk].style) {  
+            } else if (b[bk].tagName && b[bk].appendChild && b[bk].removeChild && b[bk].style) {
                 a[bk] = b[bk];
             } else {
 
@@ -475,8 +475,8 @@ var highed = {
 
                 a[bk] = a[bk] || {};
                 highed.merge(a[bk], b[bk]);
-            }          
-        });    
+            }
+        });
         return a;
     },
 
@@ -503,7 +503,7 @@ var highed = {
      *  @param {anything} what - the value to check
      *  @return {bool} - true if number
      */
-    isNum: function(what) {
+    isNum: function (what) {
         return !isNaN(parseFloat(what)) && isFinite(what);
     },
 
@@ -522,7 +522,7 @@ var highed = {
      * @return {bool} - true if array
      */
     isArr: function (what) {
-        return (!highed.isNull(what) && what.constructor.toString().indexOf("Array") > -1);
+        return (!highed.isNull(what) && what.constructor.toString().indexOf('Array') > -1);
     },
 
     /** Check if a value is a boolean
@@ -563,20 +563,22 @@ var highed = {
             helpURL: 'http://www.highcharts.com/products/highcharts-editor',
             defaultLanguage: 'en',
             includeCDNInExport: true,
-            stickyChartProperties: {}
+            stickyChartProperties: {},
+            includeHighcharts: true,
+            cloudAPIURL: 'http://127.0.0.1:4000/'
         },
         cdnScripts = [
-            "https://code.highcharts.com/stock/highstock.js",   
-            "http://code.highcharts.com/adapters/standalone-framework.js",  
-            "https://code.highcharts.com/highcharts-more.js",   
-            "https://code.highcharts.com/highcharts-3d.js", 
-            "https://code.highcharts.com/modules/data.js",  
-            "https://code.highcharts.com/modules/exporting.js"
+            'https://code.highcharts.com/stock/highstock.js',
+            'http://code.highcharts.com/adapters/standalone-framework.js',
+            'https://code.highcharts.com/highcharts-more.js',
+            'https://code.highcharts.com/highcharts-3d.js',
+            'https://code.highcharts.com/modules/data.js',
+            'https://code.highcharts.com/modules/exporting.js'
         ]
     ;
 
     ///////////////////////////////////////////////////////////////////////////
-    
+
     function pollForReady() {
         if (!isReady) {
             if (document.body) {
@@ -593,9 +595,16 @@ var highed = {
     pollForReady();
 
     ///////////////////////////////////////////////////////////////////////////
-    
+
+    /**
+     * Whitelist an option in simple view
+     */
+    highed.exposeOption = function (option) {
+
+    };
+
     /** Set/get an option
-     *  Skip `value` to get the value 
+     *  Skip `value` to get the value
      *  @param option {string} - the option to set
      *  @param value {anything} - the value to set
      *  @returns {anything} - the option value
@@ -651,7 +660,7 @@ var highed = {
         things.splice(0, 1);
 
         if (level <= currentLogLevel) {
-            console.log.apply(undefined, [logLevels[level - 1] + ':'].concat(things));
+            console.log.apply(undefined, [logLevels[level - 1] + ':'].concat(things)); //eslint-disable-line no-console
         }
     };
 
@@ -710,7 +719,7 @@ var highed = {
             n.onload = fn;
         }
 
-        document.head.appendChild(n);        
+        document.head.appendChild(n);
     };
 
     /** Returns true if running on a phone
@@ -723,8 +732,8 @@ var highed = {
 
     function checkIfPhone() {
         var check = false;
-        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;        
+        (function (a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
     }
 
     isOnPhone = checkIfPhone();
@@ -745,9 +754,9 @@ var highed = {
     //             var s = document.createElement('script');
     //             s.src = script;
     //             document.head.appendChild(s);
-    //         });            
+    //         });
     //     } else {
-    //         window.setTimeout(tryAddScripts, 10);            
+    //         window.setTimeout(tryAddScripts, 10);
     //     }
     // }
 
@@ -770,7 +779,7 @@ var highed = {
 
     // var inc = {},
     //     incl = []
-    // ; 
+    // ;
 
     // document.querySelectorAll("script").forEach(function(t) {inc[t.src.substr(0, t.src.indexOf("?"))] = 1;});
 
