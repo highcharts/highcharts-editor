2eddec69d4c9d1219cf6137f2275ba56f135aed6
Kulbir Kainth <kai246@hotmail.co.uk>
2019-05-14 09:33:48 +0200

    Included accessibility module in embed js

--- src/ui/highed.chartpreview.js
+++ src/ui/highed.chartpreview.js
@@ -1738,6 +1738,7 @@ highed.ChartPreview = function(parent, attributes) {
           'https://code.highcharts.com/modules/exporting.js',
           'https://code.highcharts.com/modules/funnel.js',
           'https://code.highcharts.com/6.0.2/modules/annotations.js',
+          'https://code.highcharts.com/modules/accessibility.js',
           // 'https://code.highcharts.com/modules/series-label.js'
           'https://code.highcharts.com/modules/solid-gauge.js'
         ],
